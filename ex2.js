3c0186f37cdcde626b1e84f839811f026a85f96b
epili50 <epili50@gmail.com>
2024-04-23 20:12:20 +0200

    Realizado 2 y 3

--- ex2.js
+++ ex2.js
@@ -6,8 +6,8 @@ function suma(a, b) {
   return a + b;
 }
 
-let sum1; //MODIFICA SOLO ESTA LÍNEA
-let sum2; // MODIFICA SOLO ESTA LÍNEA
+let sum1 = 2; //MODIFICA SOLO ESTA LÍNEA
+let sum2 = 3; // MODIFICA SOLO ESTA LÍNEA
 let resultado = suma(sum1, sum2);
 console.log(resultado);
 
