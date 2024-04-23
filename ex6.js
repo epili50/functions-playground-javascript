f67c713bdd029805f22f2bb95143948a8102c6cc
epili50 <epili50@gmail.com>
2024-04-23 20:31:05 +0200

    Realizado ex6

--- ex6.js
+++ ex6.js
@@ -6,7 +6,12 @@
  * Modifica únicamente la función para conseguirlo.
  */
 function isUpperCase(word) {
-  return true;
+  if(word == word.toUpperCase()){
+    return true;
+  }else{
+    return false;
+  }
+  
 }
 
 /**
